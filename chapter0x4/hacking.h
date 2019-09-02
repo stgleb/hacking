35f6bc19221dd35ea7158e7bdb9f0e7e3988096c
stgleb <glebstepanov1992@gmail.com>
2019-09-02 19:00:03 +0300

    add more exploits

---
+++ chapter0x4/hacking.h
@@ -0,0 +1,26 @@
+// Dumps raw memory in hex bytes and printable split format
+void dump(const unsigned char* data_buffer, const unsigned int length) {
+    unsigned char byte;
+    unsigned int i;
+    unsigned int j;
+
+    for(i=0;i < length; i++) {
+        byte = data_buffer[i];
+        // Display byte in hex
+        printf("%02x ", data_buffer[i]);
+        if((i% 16 == 15) || (i == length - 1)) {
+            for(j = 0;j < 15 - (i % 16); j++)
+                printf("  ");
+            printf("| ");
+            for(j=(i - (i % 16));j <= i; j++) {
+                byte=data_buffer[j];
+                if(byte > 31 && (byte < 127)) {
+                    printf("%c", byte);
+                } else {
+                    printf(".");
+                }
+            }
+            printf("\n");
+        }
+    }
+}
